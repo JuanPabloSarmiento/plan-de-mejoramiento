000633eb571f688348e8a586b5f479d237162149
JuanPablo <juanpnavarrete0@gmail.com>
2025-07-08 15:31:26 -0500

    'wweeeenas'

--- planMejoramiento/settings.py
+++ planMejoramiento/settings.py
@@ -129,4 +129,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'juanpnavarrete0@gmail.com'
-EMAIL_HOST_PASSWORD = 'wqha btdo chjc lisq'
+EMAIL_HOST_PASSWORD = ''
